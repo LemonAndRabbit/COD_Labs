58ce71e38cd8e65c29106303099476e0530bb064
LemonAndRabbit <rabbitforyou@foxmail.com>
2020-05-12 21:40:46 +0800

    deleted useless files

--- Lab2_CPU.srcs/sources_1/new/Add.v
+++
@@ -1,7 +0,0 @@
-module Add(
-    input [31:0] oprand1,
-    input [31:0] oprand2,
-    output [31:0] opout
-    );
-    assign opout = oprand1 + oprand2;
-endmodule
--- lab3_dbu.srcs/sources_1/new/Ins_mem.v
+++
@@ -1,6 +0,0 @@
-module Instruction_Memory(
-    input [31:0] Read_address,
-    output [31:0] Instruction
-    );
-    dist_mem_gen_ins ins_dist_mem(.a(Read_address[9:2]),.spo(Instruction));
-endmodule
